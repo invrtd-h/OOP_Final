ffb4a8e9b586354c2b33ab327ad49359f007d452
Hyegeun Song <hyegeun_song@gm.gist.ac.kr>
2022-06-03 02:58:37 +0900

    Initial Commit

---
+++ OOP_Final/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP_Final
+//
+//  Created by 송혜근 on 2022/06/03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
